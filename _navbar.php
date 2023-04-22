7845d5ecb78453d1c37a71c7e3884754b894bcb1
kfcampit <kyle_campit@dlsu.edu.ph>
2023-04-22 13:17:28 +0800

    Update _navbar.php

--- _navbar.php
+++ _navbar.php
@@ -84,6 +84,28 @@
         if ($_SERVER['SCRIPT_NAME'] == '/index.php' || $_SERVER['SCRIPT_NAME'] == '/dts.php' || $_SERVER['SCRIPT_NAME'] == '/view.php' || $_SERVER['SCRIPT_NAME'] == '/actDetails.php' || $_SERVER['SCRIPT_NAME'] == '/accountManagement.php' ) {
             
         } else header('Location: /restrict.php');
+    } else if ($_SESSION['classification'] == "ORGOFFICER") {
+        echo '
+        <nav class="navbar sticky-top navbar-dark bg-primary navbar-expand-lg" style="margin-bottom: 32px">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="index.php">CSO - APSMS</a>
+            <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                <a class="nav-link active" href="dts.php">Document Tracking System</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link active" href="actDetails.php">Activity Receiving and Tracking System</a>
+                </li>
+            </ul>
+            <span class="navbar-text active"> <?php echo $_SESSION["accountName"] ?> </span>
+            </div>
+        </div>
+        </nav>
+        ';
+        if ($_SERVER['SCRIPT_NAME'] == '/index.php' || $_SERVER['SCRIPT_NAME'] == '/dts.php' || $_SERVER['SCRIPT_NAME'] == '/view.php' || $_SERVER['SCRIPT_NAME'] == '/actDetails.php' ) {
+            
+        } else header('Location: /restrict.php');
     }
     else {
         echo '
